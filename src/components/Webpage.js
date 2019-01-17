e953e07fd26bff58fb8fe2e83ca7f78d7e6b906c
Zachary Baca <cubsfan.46350@gmail.com>
2019-01-17 21:07:17 +0000

    Automatically backed up by Learn

---
+++ src/components/Webpage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export class Webpage extends React.Component {
+  render() {
+    return (
+      <h1>The world's coolest webpage</h1>
+      );
+  }
+}
